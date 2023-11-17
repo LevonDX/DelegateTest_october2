b79ff2aac440b10c80baf8e59e4c157d6d9f8bb7
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-11-17 09:44:22 +0400

    Add project files.

---
+++ DelegateTest_october2/Extensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTest_october2
+{
+	// Func - returns T
+	// Action - returns void
+	// Predicate - returns bool
+
+	static class Extensions
+	{
+		public static IEnumerable<T> GetElementsByCondition<T>(this IEnumerable<T> array, 
+			Predicate<T> condition)
+		{
+			foreach (T element in array)
+			{
+				if (condition(element))
+				{
+					yield return element;
+				}
+			}
+		}		
+
+		public static void Show<T>(this IEnumerable<T> array)
+		{
+			foreach (T element in array)
+				Console.WriteLine(element);
+		}
+	}
+}
---
+++ DelegateTest_october2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DelegateTest_october2
+{
+	internal class Program
+	{
+		static IEnumerable<int> ShowNumbers()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				yield return i;
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			int[] array = { 3, 3, 2, 1, 4, 5, 6 };
+
+			// LINQ - Language INtegrated Query, Functional Programming
+
+			//int result = ShowNumbers().Where(a => (a * x) % 2 == 0).SkipWhile(a => a < 7).Max();
+
+			//var list = ShowNumbers().Where(a => a % 2 == 0).ToList();
+
+			// list = ShowNumbers().ToList().Where(a => a % 2 == 0);
+
+			int x = 10;
+
+			var f = array.Where(a =>
+			{
+				x++;
+				return a % 2 == 0;
+
+			});
+
+			Console.WriteLine(x);
+
+
+			f.Max();
+
+			Console.WriteLine(x);
+		}
+	}
+}
